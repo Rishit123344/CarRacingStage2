71a42cdeef232f43182f41899ade1cf54f3fd070
Rishit123344 <bramarasunku@gmail.com>
2020-02-16 12:22:23 +0530

    marked players

--- js/Form.js
+++ js/Form.js
@@ -5,6 +5,7 @@ class Form {
     this.button = createButton('Play');
     this.greeting = createElement('h2');
     this.title = createElement('h2');
+    this.reset = createButton('reset');
   }
   hide(){
     this.greeting.hide();
@@ -16,10 +17,15 @@ class Form {
   display(){
     this.title.html("Car Racing Game");
     this.title.position(displayWidth/2 - 50, 0);
+this.reset.position(displayWidth-100,20);
+this.reset.mousePressed(()=>{
+player.updateCount(0);
+game.update(0);
+})
 
     this.input.position(displayWidth/2 - 40 , displayHeight/2 - 80);
     this.button.position(displayWidth/2 + 30, displayHeight/2);
-2
+
     this.button.mousePressed(()=>{
       this.input.hide();
       this.button.hide();
